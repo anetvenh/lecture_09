72eb03005ac8d754288eba08bb8b3e971c4c0bbd
Aneta Venhudova <aneta.venhudova@seznam.cz>
2022-04-11 12:32:35 +0200

    doplneni funkce linear_search pro hledani cisel

--- searching.py
+++ searching.py
@@ -19,10 +19,33 @@ def read_data(file_name, field):
         return data[field]
 
 
-def main():
-    unordered_numbers = read_data("sequential.json", "unordered_numbers")
-    print(unordered_numbers)
+def linear_search(unordered_nbrs, searched_nbr):
+    """
+    searches numbers in sequence and counts their frequency
 
+    :param unordered_nbrs: (list) list of unordered numbers to search searched number in
+    :param searched_nbr: (int) searched number
+    :return output: (dict) dictionary with indexes of searched number as key and frequency as value
+    """
+
+    positions = []
+    count = 0
+
+    for i, nbr in enumerate(unordered_nbrs):
+        if nbr == searched_nbr:
+            positions.append(i)
+            count += 1
+
+    output = dict()
+    output["positions"] = positions
+    output["count"] = count
+    # output = {"positions" : positions, "count" : count}
+    return output
+
+
+def main():
+    sequence = read_data("sequential.json", "unordered_numbers")
+    print(linear_search(sequence, 9))
 
 if __name__ == '__main__':
     main()
